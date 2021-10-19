0caccb23272b0f569227d5f87d4eaccd4066d938
abigailknowles <abigailknowlesx@yahoo.co.uk>
2021-10-19 13:27:53 +0100

    created app, delete unneccesary pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './pages/HomePage';
-import './pages/LoginPage';
-import './pages/ManageAccount';
-import './pages/CreatePatientAccount';
-import './pages/EditPatientAccount';
-import './pages/DeletePatientAccount';
-import './pages/PatientAnalytics';
 
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import ManageAccount from './pages/ManageAccount';
-import CreatePatientAccount from './pages/CreatePatientAccount';
-import DeletePatientAccount from './pages/DeletePatientAccount';
-import EditPatientAccount from './pages/EditPatientAccount';
-import PatientAnalytics from './pages/PatientAnalytics';
 
 class App extends Component {
   render() {
@@ -25,13 +13,7 @@ class App extends Component {
       <Router>
         <div>
           <Switch>
-            <Route exact path='/' render={(props) => <LoginPage {...props} />} />
-            <Route exact path='/home' render={(props) => <HomePage {...props} />} />
-            <Route exact path='/create-account' render={(props) => <CreatePatientAccount {...props} />} />
-            <Route exact path='/edit-account' render={(props) => <EditPatientAccount {...props} />} />
-            <Route exact path='/delete-account' render={(props) => <DeletePatientAccount {...props} />} />
-            <Route exact path='/patient-analytics' render={(props) => <PatientAnalytics {...props} />} />
-            <Route path='/manage-account' render={(props) => <ManageAccount {...props} />} />
+            <Route exact path='/' render={(props) => <HomePage {...props} />} />
           </Switch>
         </div>
       </Router>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
-import { Button, Col, Row, Container } from 'react-bootstrap';
-import { withRouter, NavLink } from 'react-router-dom';
+import { Col, Row, Container } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
 import MainMenu from '../components/MainMenu';
 
 
@@ -12,55 +12,9 @@ function HomePage() {
         <Row>
           <Col sm={1}></Col>
           <Col sm={5}>
-            <h3 className="home-page-header">Welcome back!</h3>
+            <h3 className="home-page-header">Data Visulisation</h3>
           </Col>
           <Col sm={5}>
-          </Col>
-          <Col sm={1}></Col>
-
-        </Row>
-        <Row>
-          <Col sm={1}></Col>
-          <Col sm={5}>
-            <NavLink to="/create-account">
-              <Button variant="light" className="home-page-buttons">
-                <h3 className="home-page-buttons-text">
-                  Create Patient Account
-                </h3>
-              </Button>
-            </NavLink>
-          </Col>
-          <Col sm={5}>
-            <NavLink to="/edit-account">
-              <Button variant="light" className="home-page-buttons">
-                <h3 className="home-page-buttons-text">
-                  Edit Patient Account
-                </h3>
-              </Button>
-            </NavLink>
-          </Col>
-          <Col sm={1}></Col>
-        </Row>
-        <Row className="padding"></Row>
-        <Row>
-          <Col sm={1}></Col>
-          <Col sm={5}>
-            <NavLink to="/delete-account">
-              <Button variant="light" className="home-page-buttons">
-                <h3 className="home-page-buttons-text">
-                  Delete Patient Account
-                </h3>
-              </Button>
-            </NavLink>
-          </Col>
-          <Col sm={5}>
-            <NavLink to="/patient-analytics">
-              <Button variant="light" className="home-page-buttons">
-                <h3 className="home-page-buttons-text">
-                  Patient Analytics
-                </h3>
-              </Button>
-            </NavLink>
           </Col>
           <Col sm={1}></Col>
         </Row>
