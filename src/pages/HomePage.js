a8711b2c287b5a6a133086c3bacf4ded9f3fa54f
abigailknowles <abigailknowlesx@yahoo.co.uk>
2021-12-15 19:56:13 +0000

    practising using d3Js with react

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,36 @@
-import { Col, Row, Container } from 'react-bootstrap';
-import { withRouter } from 'react-router-dom';
+import React, { useState } from "react";
 import MainMenu from '../components/MainMenu';
 
+const Circles = () => {
+  const generateDataset = () => (
+    Array(10).fill(0).map(() => ([
+      Math.random() * 80 + 10,
+      Math.random() * 35 + 10,
+    ]))
+  )
+  const [dataset, setDataset] = useState(
+    generateDataset()
+  )
 
-function HomePage() {
+
+  const useInterval = (() => {
+    const newDataset = generateDataset()
+    setDataset(newDataset)
+  }, 2000)
+
   return (
     <>
       <MainMenu></MainMenu>
-
-      <Container>
-        <Row>
-          <Col sm={1}></Col>
-          <Col sm={5}>
-            <h3 className="home-page-header">Data Visulisation</h3>
-          </Col>
-          <Col sm={5}>
-          </Col>
-          <Col sm={1}></Col>
-        </Row>
-      </Container>
+      <svg viewBox="0 0 100 50">
+        {dataset.map(([x, y], i) => (
+          <circle
+            cx={x}
+            cy={y}
+            r="3"
+          />
+        ))}
+      </svg>
     </>
-  );
+  )
 }
-
-export default withRouter(HomePage);
+export default Circles;
