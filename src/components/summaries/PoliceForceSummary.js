055f4ba10c6688b43a50291d0fa0338cc0275515
abigailknowles <abigailknowlesx@yahoo.co.uk>
2022-03-10 17:22:25 +0000

    added arrest count functionality

--- src/components/summaries/PoliceForceSummary.js
+++ src/components/summaries/PoliceForceSummary.js
@@ -11,11 +11,40 @@ class PoliceForceSummary extends Component {
     this.state = {
       largestPoliceForce: "Metropolitan Police",
     }
-
-
   }
 
-  exists(key, array) {
+  reasonExists(key, array) {
+    var isFound = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].key === key) {
+        isFound = true;
+      }
+    }
+    return isFound;
+  }
+
+  getReasonsByKey(key, arr) {
+    var array = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].object_of_search === key) {
+        array.push(arr[i]);
+      }
+    }
+    return { outcomes: array, count: array.length };
+  }
+
+  groupReasons(arr) {
+    var groups = [];
+
+    for (var i = 0; i < arr.length; i++) {
+      var key = arr[i].object_of_search;
+      if (this.reasonExists(key, groups) === false)
+        groups.push({ key: key, outcomes: this.getReasonsByKey(key, arr) })
+    }
+
+    return { groups: groups, count: arr.length };
+  }
+  outcomeExists(key, array) {
     var isFound = false;
     for (let i = 0; i < array.length; i++) {
       if (array[i].key === key) {
@@ -40,7 +69,7 @@ class PoliceForceSummary extends Component {
 
     for (var i = 0; i < arr.length; i++) {
       var key = arr[i].outcome;
-      if (this.exists(key, groups) === false)
+      if (this.reasonExists(key, groups) === false)
         groups.push({ key: key, outcomes: this.getByOutcomeKey(key, arr) })
     }
 
@@ -51,21 +80,15 @@ class PoliceForceSummary extends Component {
     fetch(`https://data.police.uk/api/stops-force?force=bedfordshire`)
       .then(res => res.json())
       .then((data) => {
-        var labels = [];
-        var result = this.groupOutcomes(data);
-        var outcome = this.getMostCommonOutcome(result.groups)
+        var result = this.groupReasons(data);
+        var count = this.groupOutcomes(data);
+        var reason = this.getMostCommonReason(result.groups)
         this.setState({
           isLoaded: true,
           stopSearchResult: data,
-          mostCommonOutcome: outcome.key,
-          groupedData: result,
-          total: data.length,
-          isShown: true,
-          options: {
-            labels: labels
-          }
+          mostCommonReason: reason.key,
+          arrestCount: count.groups[0].outcomes.count
         });
-        console.log(this.state.mostCommonOutcome.key)
       },
         (error) => {
           this.setState({
@@ -89,14 +112,7 @@ class PoliceForceSummary extends Component {
     return sortedArray[sortedArray.length - 1];
 
   }
-
-  getMostCommonOutcome(groups) {
-    var sortedArray = groups.sort((elmement1, element2) => {
-
-      return elmement1.outcomes.count - element2.outcomes.count;
-    });
-
-    return sortedArray[sortedArray.length - 1];
+  getArrestCount() {
 
   }
 
@@ -121,11 +137,11 @@ class PoliceForceSummary extends Component {
         </Row>
         <Row>
           <FontAwesomeIcon size="1x" className="download-icon" icon={faBars} />
-          <h5 className="summary-text">The most common reason for stop and search is {this.state.mostCommonOutcome} </h5>
+          <h5 className="summary-text">The most common reason for stop and search is {this.state.mostCommonReason} </h5>
         </Row>
         <Row>
           <FontAwesomeIcon size="1x" className="download-icon" icon={faBan} />
-          <h5 className="summary-text">Stop and search resulted in arrests 500</h5>
+          <h5 className="summary-text">Stop and search resulted in arrests {this.state.arrestCount}</h5>
         </Row>
       </>
     );
